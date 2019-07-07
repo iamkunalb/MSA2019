95a13ba21c33f913324df5a7a4b6336e89dd59ea
iamkunalb <knlbht1@gmail.com>
2019-07-07 19:44:02 +1200

    Worls

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h1 className='headerText'>What's my age?</h1>
+        </div>
+    )
+}
